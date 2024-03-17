7c38532fd1f5e0e49529fffe8e45e5828bbf2d26
mggger <mggis0or1@gmail.com>
2024-03-17 17:15:26 +0800

    update

--- src/components/analyse.js
+++ src/components/analyse.js
@@ -18,7 +18,7 @@ export default function GoogleAnalytics() {
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID});
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
                 `}
             </Script>
         </div>
--- src/components/haeder.js
+++ src/components/haeder.js
@@ -1,25 +1,71 @@
 import React from 'react';
+import Head from 'next/head'
+import GoogleAnalytics from "@/components/analyse";
+
+const CustomHead = () => {
+    const siteTitle = 'AI Calorie Calculator | Food Recognition'
+
+    const description = 'Utilize AI technology to recognize food images and generate calorie statistics, helping you better manage your dietary health.'
+    const pageImage = 'https://cc.gptdevelopment.online/cor.webp';
+    const keywords = 'AI, food recognition, calorie counting, calorie statistics， Calorie Calculator， AI Calorie Calculator， Do Calorie Calculator， '
+
+
+    return (
+        <Head>
+            <title>{siteTitle}</title>
+            <meta name="description" content={description}/>
+            <meta property="og:title" content={siteTitle}/>
+            <meta name="twitter:title" content={siteTitle}/>
+            <meta itemprop="name" content={siteTitle}/>
+            <link rel="apple-touch-icon" sizes="180x180" href="https://cc.gptdevelopment.online/favicon.ico"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="https://cc.gptdevelopment.online/favicon.ico"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="https://cc.gptdevelopment.online/favicon.ico"/>
+            <link rel="shortcut icon" href="https://cc.gptdevelopment.online/favicon/favicon.ico" type="image/x-icon"/>
+            <link rel="canonical" href="https://cc.gptdevelopment.online/"/>
+            <meta name="keywords" content={keywords}/>
+            <meta name="application-name" content={siteTitle}/>
+            <meta property="og:description" content={description}/>
+            <meta property="og:site_name" content={siteTitle}/>
+            <meta property="og:url" content="https://cc.gptdevelopment.online/"/>
+            <meta property="og:locale" content="en_US"/>
+            <meta property="og:image" content={pageImage}/>
+            <meta property="og:image:secure_url" content={pageImage}/>
+            <meta property="og:type" content="website"/>
+            <meta name="twitter:card" content={siteTitle}/>
+            <meta name="twitter:site" content="https://cc.gptdevelopment.online/"/>
+            <meta name="twitter:image" content={pageImage}/>
+            <meta name="twitter:description" content={description}/>
+        </Head>
+    )
+}
+
 
 export const Header = () => {
     return (
-        <header className="bg-base-100 shadow-lg">
-            <div className="container mx-auto flex justify-between items-center p-5">
-                {/* Logo Section */}
-                <div className="flex items-center">
-                    <img src="/health.svg" alt="Logo" className="mr-3 h-6 sm:h-9" />
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Calorie Calculator</span>
+        <div>
+            <CustomHead/>
+            <GoogleAnalytics/>
+            <header className="bg-base-100 shadow-lg">
+                <div className="container mx-auto flex justify-between items-center p-5">
+                    {/* Logo Section */}
+                    <div className="flex items-center">
+                        <img src="/health.svg" alt="Logo" className="mr-3 h-6 sm:h-9"/>
+                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Calorie Calculator</span>
+                    </div>
+
+                    {/* Navigation Menu */}
+                    <nav className="hidden md:flex space-x-10">
+                        <a href="https://github.com/mggger/Calorie-Calculator" target="_blank" rel="noopener noreferrer"
+                           className="text-gray-700 hover:text-gray-900">
+                            <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                                <path fillRule="evenodd" clipRule="evenodd"
+                                      d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.775.418-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
+                            </svg>
+                        </a>
+                    </nav>
+
                 </div>
-
-                {/* Navigation Menu */}
-                <nav className="hidden md:flex space-x-10">
-                    <a href="https://github.com/mggger/Calorie-Calculator" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-gray-900">
-                        <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                            <path fillRule="evenodd" clipRule="evenodd" d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.775.418-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
-                        </svg>
-                    </a>
-                </nav>
-
-            </div>
-        </header>
+            </header>
+        </div>
     );
 };
