9095af179e3c3b57c7ea276997f38e5ee6206e12
mggger <mggis0or1@gmail.com>
2024-03-17 17:58:51 +0800

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,7 +9,6 @@ export default function App({ Component, pageProps }) {
         <Layout>
             <Component {...pageProps} />
         </Layout>
-            <GoogleAnalytics />
         </>
     );
 }
