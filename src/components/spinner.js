ceee24ff2ca4015e667bddbee2f5d32c65fa31c7
mggger <mggis0or1@gmail.com>
2024-03-18 10:25:16 +0800

    update

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 
 const LoadingSpinner = () => (
-    <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-white bg-opacity-50 z-50">
+    <div style={{ height: `calc(100vh - 500px)` }}
+        className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-white bg-opacity-50 z-50" >
         <div className="three-body">
             <div className="three-body__dot"></div>
             <div className="three-body__dot"></div>
